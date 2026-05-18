18d953823e15c8051a575df1be67a80a518dee14
Kaan Demiroz <kaandemiroz@gmail.com>
2026-05-18 02:52:40 -0700

    Keep both methods of adding DBM counts for classic compatibility. Updated TOC for 12.0.7

--- DBM-CountPack-Asmongold/CountLoadList.lua
+++ DBM-CountPack-Asmongold/CountLoadList.lua
@@ -2,6 +2,12 @@ local inserted = false
 
 function DBMCPAsmongold()
 	if inserted then return end
+
+	-- New method for Midnight
 	DBM:AddCountSound("Asmongold", "Asmongold", "Interface\\AddOns\\DBM-CountPack-Asmongold\\Asmongold\\", 5, true)
+
+	-- Legacy method for Classic support
+	tinsert(DBM.Counts, {	text	= "Asmongold",	value 	= "Asmongold", path = "Interface\\AddOns\\DBM-CountPack-Asmongold\\Asmongold\\", max = 5})
+
 	inserted = true
 end
